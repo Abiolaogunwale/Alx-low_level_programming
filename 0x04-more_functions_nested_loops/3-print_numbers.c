371251970acc3c027aee307620116bb1c08da5b5
Abiola <ajayiabiola79@yahoo.com>
2023-02-25 07:30:22 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,23 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
-* mul - multiplies two integers
-* @a: first integer
-* @b: second integer
-*
-* Description: Write a function that multiplies two integers.
-* Prototype: int mul(int a, int b);
-*
-* Return: addition of a and b
+ * print_numbers - Entry point
+ *
+ * Description: Write a function that prints the numbers,
+ * from 0 to 9, followed by a new line.
+ * Prototype: void print_numbers(void);
+ * You can only use _putchar twice in your code
+ *
+ * Return: void
 */
 
-int mul(int a, int b)
+void print_numbers(void)
 {
-	return (a * b);
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 }
