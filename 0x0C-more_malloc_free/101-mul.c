d750bfac7cae8c9a3cce17332a81fec93c8cddd9
Abiola <ajayiabiola79@yahoo.com>
2023-03-16 04:30:08 -0700

    compiled

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,4 @@
-#include "main.h
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
 #define ERR_MSG "Error"
@@ -36,14 +36,9 @@ int _strlen(char *s)
 {
 	int i = 0;
 
-
-
 	while (s[i] != '\0')
-
 	{
-
 		i++;
-
 	}
 
 	return (i);
@@ -53,22 +48,14 @@ int _strlen(char *s)
 
 
 /**
-
  * errors - handles errors for main
-
  */
 
 void errors(void)
-
 {
-
 	printf("Error\n");
-
 	exit(98);
-
 }
-
-
 
 /**
  * main - multiplies two positive numbers
@@ -88,66 +75,43 @@ int main(int argc, char *argv[])
 		errors();
 
 	len1 = _strlen(s1);
-
 	len2 = _strlen(s2);
-
 	len = len1 + len2 + 1;
-
 	result = malloc(sizeof(int) * len);
-
 	if (!result)
-
 		return (1);
 
 	for (i = 0; i <= len1 + len2; i++)
-
 		result[i] = 0;
-
 	for (len1 = len1 - 1; len1 >= 0; len1--)
-
 	{
-
 		digit1 = s1[len1] - '0';
-
 		carry = 0;
-
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-
 		{
-
 			digit2 = s2[len2] - '0';
-
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-
 			result[len1 + len2 + 1] = carry % 10;
-
 			carry /= 10;
-
 		}
 
 		if (carry > 0)
-
 			result[len1 + len2 + 1] += carry;
-
 	}
 
 	for (i = 0; i < len - 1; i++)
-
 	{
-
 		if (result[i])
-
 			a = 1;
 
 		if (a)
-
 			_putchar(result[i] + '0');
 
 	}
 
 	if (!a)
 		_putchar('0');
-	_putchar('\n')
+	_putchar('\n');
 	free(result);
 	return (0);
 }
