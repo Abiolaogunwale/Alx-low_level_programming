32a613592b0bbdfe8823d735d16ffafcc8a7673c
Abiola <ajayiabiola79@yahoo.com>
2023-03-16 15:36:50 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -70,7 +70,6 @@ int main(int argc, char *argv[])
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
@@ -95,7 +94,7 @@ int main(int argc, char *argv[])
 			_putchar(result[i] + '0');
 	}
 	if (!a)
-		_putchar('0');
+	_putchar('0');
 	_putchar('\n');
 	free(result);
 	return (0);
