656d6e27a062d8e89e48856f6e828b0d05fc6262
Abiola <ajayiabiola79@yahoo.com>
2023-03-23 07:39:05 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,10 +2,10 @@
 
 /**
  * print_all - prints anything.
- * @format: a list of types of arguments passed to the function
+ * @format: a list of types of arguments passed to the function.
+ *
  * Return: no return.
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list valist;
@@ -21,7 +21,7 @@ void print_all(const char * const format, ...)
 		{
 			if (format[i] == t_arg[j] && c)
 			{
-				printf(",");
+				printf(", ");
 				break;
 			} j++;
 		}
@@ -31,16 +31,16 @@ void print_all(const char * const format, ...)
 			printf("%c", va_arg(valist, int)), c = 1;
 			break;
 		case 'i':
-	printf("%d", va_arg(valist, int)), c = 1;
+			printf("%d", va_arg(valist, int)), c = 1;
 			break;
 		case 'f':
-	printf("%f", va_arg(valist, double)), c = 1;
+			printf("%f", va_arg(valist, double)), c = 1;
 			break;
 		case 's':
 			str = va_arg(valist, char *), c = 1;
 			if (!str)
 			{
-			printf("(nil)");
+				printf("(nil)");
 				break;
 			}
 			printf("%s", str);
