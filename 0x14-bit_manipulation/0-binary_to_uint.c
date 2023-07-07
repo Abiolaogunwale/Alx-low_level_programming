aef2a0a2143ca11ba4f2213f57987568ea67ecf1
Abiolaogunwale <ajayiabiola79@yahoo.com>
2023-07-07 11:58:46 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,16 +11,15 @@ unsigned int binary_to_uint(const char *b)
 unsigned int dec_val;
 unsigned int g;
 
-for (dec_val = 0, g = 0; b[g] != '\0'; g++)
-{
-	if (b[g] == '1')
-		dec_val = (dec_val << 1) | 1;
-	else if (b[g] == '0')
-		dec_val <<= 1;
-	else if (b[g] != '0' && b[g] != '1')
+	if (!b)
 		return (0);
-dec_val = 2 * dec_val + (b[g] - '0');
-}
 
-return (dec_val);
+	for (g = 0; b[g]; g++)
+	{
+		if (b[g] < '0' || b[g] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[g] - '0');
+	}
+
+	return (dec_val);
 }
